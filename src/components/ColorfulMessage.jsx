9fcb2da71da6be12e68027d0054a46d528ce1ce5
manjoe <dibw0216@yahoo.co.jp>
2021-05-08 05:52:34 +0900

    44 props 分割代入でコードを見やすくする

--- src/components/ColorfulMessage.jsx
+++ src/components/ColorfulMessage.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 
 const ColorfulMessage = (props) => {
+  const { color, children } = props;
   console.log(props);
   const contentStyle = {
-    color: props.color,
+    // color: color,
+    // property名と変数名が同じ場合、省略できる
+    color,
     fontSize: "18px"
   };
-  return <p style={contentStyle}>{props.children}</p>;
+  return <p style={contentStyle}>{children}</p>;
 };
 export default ColorfulMessage;
